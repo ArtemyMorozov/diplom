4739ebed9e5afa7cca916b9c264068145e5f9b19
Artemy <artemy.morozov2003@yandex.ru>
2025-03-31 16:41:26 +0300

    exit_button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,13 @@
         private void InitializeComponent()
         {
             this.con1 = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // con1
             // 
             this.con1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.con1.Location = new System.Drawing.Point(580, 396);
+            this.con1.Location = new System.Drawing.Point(12, 12);
             this.con1.Name = "con1";
             this.con1.Size = new System.Drawing.Size(191, 42);
             this.con1.TabIndex = 0;
@@ -42,12 +43,23 @@
             this.con1.UseVisualStyleBackColor = true;
             this.con1.Click += new System.EventHandler(this.con1_Click);
             // 
+            // exit
+            // 
+            this.exit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.exit.Location = new System.Drawing.Point(711, 389);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(77, 49);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exit);
             this.Controls.Add(this.con1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.HelpButton = true;
@@ -61,6 +73,7 @@
         #endregion
 
         private System.Windows.Forms.Button con1;
+        private System.Windows.Forms.Button exit;
     }
 }
 
